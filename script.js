eec6073933db49f40ed4777e287ee2493339c96f
Rani Dangari <140511941+Rani-dangari@users.noreply.github.com>
2023-08-11 20:42:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// cart
+let carticon=document.querySelector("#cart-icon");
+let cart=document.querySelector(".cart");
+let closecart=document.querySelector('#close cart');
+
+carticon.onclick=() =>{
+  cart.classList.add("active");
+
+};
+closecart.onclick=() =>{
+    cart.classList.add("active");
+  
+  };
